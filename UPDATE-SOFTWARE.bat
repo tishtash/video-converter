04285f04aff1547fcc2255b1a9449fb26d3752c4
Nick Duggal <nitishcan_1@yahoo.com>
2019-02-23 15:50:36 -0800

    Updaed software update file

--- UPDATE-SOFTWARE.bat
+++ UPDATE-SOFTWARE.bat
@@ -1,6 +1,6 @@
 PAUSE
 ECHO 'STARTING UPDATE....'
 choco install git.install
-start cmd.exe /c "git clone https://github.com/tishtash/video-converter.git temp && ROBOCOPY /E /MOVE temp ."
+start cmd.exe /c "git clone https://github.com/tishtash/video-converter.git temp && ROBOCOPY /E /MOVE temp . && npm i"
 ECHO 'PROCESSING COMPLETED'
 PAUSE
